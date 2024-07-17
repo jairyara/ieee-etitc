8cb40fb18a2135590d84b09ffd54385344d537da
Jair Yara <jair.yara11@gmail.com>
2024-07-16 20:21:17 -0500

    feat: Se añade el header y configuraciones menores[

---
+++ src/utils/menu.ts
@@ -0,0 +1,22 @@
+export let menu = [{
+    0: {
+        name: 'Inicio',
+        url: '/'
+    },
+    1: {
+        name: 'Proyecto',
+        url: '/proyectos'
+    },
+    2: {
+        name: 'Cursos',
+        url: '/cursos'
+    },
+    3: {
+        name: 'Noticias',
+        url: '/noticias'
+    },
+    4: {
+        name: 'Precios',
+        url: '/precios'
+    }
+}];
