0ab7d34a28c7604624dea5bca4aa2c474636d5f6
Jair Yara <jair.yara11@gmail.com>
2024-07-18 19:18:37 -0500

    style: Add styles for header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'blueProject': '#00629B',
+			'greenProject': '#AFE767'
+		}
 	},
 	plugins: [],
 }
