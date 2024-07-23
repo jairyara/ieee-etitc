8dd57c356610abca1ea8daa511ab71cfd59654f2
Jair Yara <jair.yara11@gmail.com>
2024-07-23 16:31:33 -0500

    chore:(UI) Add views for menu pages and finish footer desktop view

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
 		extend: {
 			colors: {
 				'blueProject': '#00629B',
-				'greenProject': '#AFE767'
+				'greenProject': '#AFE767',
+				'greenProjectAccent': '#88C64B'
 			},
 		},
 
