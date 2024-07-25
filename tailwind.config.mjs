3e37bdf4d2587ecaaa8482737c98888d8d1d44f2
sara <saradan.vm@gmail.com>
2024-07-24 19:27:33 -0500

    feat(ui): tarjeta de enventos y actividades

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'sans': ['Noto Sans', 'sans-serif'],
+				'title': ['Libre Baskerville', 'serif']
+			},
 			colors: {
 				'blueProject': '#00629B',
 				'greenProject': '#AFE767',
