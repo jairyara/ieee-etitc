6d53e0c166e973bc3ffe557247bf823dff13d26f
Jair Yara <jair.yara11@gmail.com>
2024-07-26 19:36:25 -0500

    feat(UI): Add components in index

---
+++ src/data/chapters.ts
@@ -0,0 +1,33 @@
+interface Chapters {
+    name: string;
+    largeDescription: string;
+    smallDescription: string;
+    color: string;
+}
+
+export const chapters: Chapters[] = [
+    {
+        name: "women in engineering",
+        largeDescription: "lorem ipsum dolor sit amet",
+        smallDescription: "lorem ipsum",
+        color: "text-purple-800"
+    },
+    {
+        name: "oceanic engineering society",
+        largeDescription: "lorem ipsum dolor sit amet",
+        smallDescription: "lorem ipsum",
+        color: "text-blue-600"
+    },
+    {
+        name: "computer society",
+        largeDescription: "lorem ipsum dolor sit amet",
+        smallDescription: "lorem ipsum",
+        color: "text-yellow-400"
+    },
+    {
+        name: "robotics automation society",
+        largeDescription: "Robotics and Automation is the branch of engineering that involves the conception, design, manufacture, and operation of robots. This field overlaps with electronics, computer science, artificial intelligence, mechatronics, nanotechnology, and bioengineering.",
+        smallDescription: "Robotics and Automation for the future",
+        color: "text-red-600"
+    },
+]
