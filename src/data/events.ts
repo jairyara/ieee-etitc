f250717f81ed5815749e412d58e8a300b89161d4
Jair Yara <jair.yara11@gmail.com>
2024-07-29 18:48:50 -0500

    Feat(UI): Add events section and fix minor errors

---
+++ src/data/events.ts
@@ -0,0 +1,57 @@
+interface Events {
+    name: string;
+    description: string;
+    date: string;
+    time: string;
+    location: string;
+    link: string;
+    img: string;
+}
+
+export const events: Events[] = [
+    {
+        name: "Develop a clear vision ofwhere your business is heading",
+        description: "You’ll have a repeatable process in place that gives you the confidence to make long term business decisions.",
+        date: "21st March 2021",
+        time: "10:00 AM",
+        location: "Online",
+        link: "/eventos/",
+        img: "/assets/img/pexels.jpg"
+    },
+    {
+        name: "Develop a clear vision ofwhere your business is heading",
+        description: "You’ll have a repeatable process in place that gives you the confidence to make long term business decisions.",
+        date: "21st March 2021",
+        time: "10:00 AM",
+        location: "Online",
+        link: "/eventos/",
+        img: "/assets/img/pexels.jpg"
+    },
+    {
+        name: "Develop a clear vision ofwhere your business is heading",
+        description: "You’ll have a repeatable process in place that gives you the confidence to make long term business decisions.",
+        date: "21st March 2021",
+        time: "10:00 AM",
+        location: "Online",
+        link: "/eventos/",
+        img: "/assets/img/pexels.jpg"
+    },
+    {
+        name: "Develop a clear vision ofwhere your business is heading",
+        description: "You’ll have a repeatable process in place that gives you the confidence to make long term business decisions.",
+        date: "21st March 2021",
+        time: "10:00 AM",
+        location: "Online",
+        link: "/eventos/",
+        img: "/assets/img/pexels.jpg"
+    },
+    {
+        name: "Develop a clear vision ofwhere your business is heading",
+        description: "You’ll have a repeatable process in place that gives you the confidence to make long term business decisions.",
+        date: "21st March 2021",
+        time: "10:00 AM",
+        location: "Online",
+        link: "/eventos/",
+        img: "/assets/img/hero.webp"
+    }
+]
