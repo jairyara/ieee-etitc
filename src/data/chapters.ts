16c7ad1160b39a0d228c968eb3e5a0da2a900647
Jair Yara <jair.yara11@gmail.com>
2024-08-23 19:29:48 -0500

    feat(copy): Add text for chapters and values

--- src/data/chapters.ts
+++ src/data/chapters.ts
@@ -8,26 +8,32 @@ interface Chapters {
 export const chapters: Chapters[] = [
     {
         name: "women in engineering",
-        largeDescription: "lorem ipsum dolor sit amet",
-        smallDescription: "lorem ipsum",
+        largeDescription: "Su misión es facilitar el reclutamiento y retención de mujeres en la ingeniería y carreras científicas, a nivel mundial.",
+        smallDescription: "Grupo de afinidad que busca promover la participación de la mujer en la ingeniería y carreras científicas e inspirar a las niñas alrededor del mundo a seguir sus intereses académicos en una carrera de ingeniería.",
         color: "text-purple-800"
     },
     {
         name: "oceanic engineering society",
-        largeDescription: "lorem ipsum dolor sit amet",
-        smallDescription: "lorem ipsum",
-        color: "text-blue-600"
+        largeDescription: "Su misión es promover el conocimiento y el desarrollo de la ingeniería en el océano y en áreas relacionadas, así como la promoción de la cooperación internacional en estos campos.",
+        smallDescription: "Sociedad técnica que involucra la teoría, práctica y aplicación de la ingeniería en el océano, incluyendo la ingeniería de sistemas, la acústica, la robótica, la geotecnología y la ingeniería de vehículos submarinos.",
+        color: "text-sky-600"
     },
     {
         name: "computer society",
-        largeDescription: "lorem ipsum dolor sit amet",
-        smallDescription: "lorem ipsum",
+        largeDescription: "Su misión es proveer información técnica, científica y educativa en las áreas de computación y procesamiento de la información, a través del acceso a herramientas que permitan fortalecer las habilidades en diseño, teoría y aplicación de las ciencias de la computación.",
+        smallDescription: "Sociedad técnica que involucra toda la teoría, práctica y aplicación de la ciencia y la tecnología de la computación y el procesamiento de la información.",
         color: "text-yellow-400"
     },
     {
         name: "robotics automation society",
-        largeDescription: "Robotics and Automation is the branch of engineering that involves the conception, design, manufacture, and operation of robots. This field overlaps with electronics, computer science, artificial intelligence, mechatronics, nanotechnology, and bioengineering.",
-        smallDescription: "Robotics and Automation for the future",
+        largeDescription: "Tiene como misión fomentar el desarrollo e intercambio de conocimiento cientifico y tecnológico en el campo de la robótica y la automatización, en beneficio de sus miebros y la humanidad.",
+        smallDescription: "Sociedad técnica que busca el avance de la teoría y práctica de la robótica y la automatización, así como de las ciencias y artes relacionadas a ella.",
         color: "text-red-600"
     },
+    {
+        name: "aerospace electronic systems society",
+        largeDescription: "Tiene como misión proveer información actual y relevante que atraiga, involucre, ayude y retenga una gran cantidad de miembros a nivel mundial en el campo de la ingeniería de sistemas electrónicos integrados y sistemas integrados a gran escala.",
+        smallDescription: "Capítulo técnico dedicado al avance de sistemas electrónicos integrados y sistemas integrados a gran escala, por ejemplo sistemas diseñados para el Espacio, sistemas de telemetría entre otros.",
+        color: "text-blue-500"
+    }
 ]
--- src/data/values.ts
+++ src/data/values.ts
@@ -5,19 +5,19 @@ interface value {
 
 export const values: value[] = [
     {
-        title: "Integración",
-        description: "Buscamos la integración de todos los miembros de la rama estudiantil."
-    },
-    {
-        title: "Educación",
-        description: "Promovemos la educación y el desarrollo profesional de nuestros miembros."
-    },
-    {
-        title: "Experiencias",
-        description: "Brindamos experiencias que permitan a nuestros miembros crecer y desarrollarse."
+        title: "Innovación",
+        description: "Exploramos nuevas ideas y tecnologías que desafían lo convencional, creando soluciones que transforman la sociedad y abren nuevas posibilidades para el futuro."
     },
     {
         title: "Excelencia",
-        description: "La excelencia inspira y motiva a los miembros del IEEE a superarse constantemente y a alcanzar los más altos estándares de desempeño y calidad en sus proyectos y actividades."
-    }
+        description: "Nos esforzamos por alcanzar la excelencia en cada proyecto, compromiso y meta, promoviendo un estándar de calidad que refleja nuestra pasión por el aprendizaje y la tecnología."
+    },
+    {
+        title: "Empoderamiento",
+        description: "Fomentamos un entorno donde cada miembro se sienta capaz de expresar su potencial, liderar con responsabilidad y contribuir a cambios significativos en la sociedad."
+    },
+    {
+        title: "Educación",
+        description: "Facilitamos un aprendizaje continuo y colaborativo que no solo forma profesionales, sino también ciudadanos responsables comprometidos con el progreso científico y tecnológico."
+    },
 ]
